360a5c85b0b7c674869cb045e3a1bdd46588c56f
Adrian Sharma <sharma.adrian@gmail.com>
2014-03-21 14:55:28 -0400

    Add a full_title helper. If no :title in view, use base title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#returns the full title on a per-page basis. 
+	def full_title(page_title) 								#method definition 
+		base_title = "Ruby on Rails Tutorial Sample App" 	#variable assignment
+		if page_title.empty? 								#contional flow statement with boolean test
+			base_title										#return base_title (Implicit return)
+		else												#control flow 
+			"#{base_title} | #{page_title}"					#string interpolation
+		end
+	end
 end
